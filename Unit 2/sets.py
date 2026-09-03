460ff0ab1131a392757bee97f220a48739ea57d5
Ojas Chaudhary <ojaschaudhary.20@gmail.com>
2026-09-03 09:51:38 +0530

    some changes

--- Unit 2/sets.py
+++ Unit 2/sets.py
@@ -1,3 +1,37 @@
-A = {1,2,3}
-B = {3,4}
-print(A.union(B))
+# # A = {1,2,3}
+# # B = {3,4,5,6}
+# # print(A.difference(B))
+
+# # num = {1,2,3,4}
+# # num.add(5)
+# # print(num)
+
+# # A = {1,3}
+# # B = {1,2,3,4}
+# # print(A.(B))
+
+# # A = {1,2,3,4,5,6}
+# # A.clear()
+# # print(A)
+
+# A = [1,2,2,2,3,4,5]
+# A.remove(2)
+# print(A)
+
+
+#for set output
+# A = [1,2,2,2,2,3,4,5]
+# A = (set(A))
+# print(A)
+
+#for list output
+# A = [1,2,2,2,2,3,4,5]
+# A = list(set(A))
+# print(A)
+
+# A = dict(name="Rahul", age=20)
+# A = ["Gita", "ABC"]
+# print(A)
+
+A = dict(name="rahul", age=20)
+print(A[0])
